aa969052266659fff39a16f6703c23ec17eaa8f0
Elias Melendez Portillo <emelendez@ranchonatoches.com.mx>
2024-01-25 17:44:45 -0700

    fix bressenham

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var color = "#000000";
 var grosor = 1;
 var drawing = false;
 
-var modos = ["linea", "lapiz", "borrar","cuadrado"];
 var gridEnabled = false;
 var startPoint = null;
 var endPoint = null;
@@ -125,103 +124,106 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     });
+    
+// LINEA BRESENHAM
+function drawLine(start, end) {
+    // Algoritmo de Bresenham
+    
+    // Calcular la distancia entre los dos puntos
+    var dx = Math.abs(end.x - start.x);
+    var dy = Math.abs(end.y - start.y);
+    
+    var sx = (start.x < end.x) ? 1 : -1; // si el punto inicial es menor que el punto final entonces sx = 1, de lo contrario sx = -1
+    var sy = (start.y < end.y) ? 1 : -1; // si el punto inicial es menor que el punto final entonces sy = 1, de lo contrario sy = -1
+    // Calcular el pk
+    var pk = dx - dy;
 
-    //LINEA  BRESENHAM
-    function drawLine(start, end) {
-        // Algoritmo de Bresenham
-        //Calcular la distancia entre los dos puntos
-        var dx = Math.abs(end.x - start.x); //Δx = |x2 - x1|
-        var dy = Math.abs(end.y - start.y); //Δy = |y2 - y1|
-        
-        //Calcular el punto medio
-        var pk = (2*dy) - dx; //e = 2Δy - Δx
+    while (true) {
+        // Dependiendo del grosor, dibujar más píxeles alrededor del punto
 
-        /*
-            En cada xk,a lo largo de la línea, que inicia en k=0, se efectúa la prueba siguiente: 
-            si pk <0, el siguiente punto que se debe trazar es (xk +1, yk) y  pk +1 = pk + 2Δy. 
-            De otro modo, el siguiente punto que se debe trazar es (xk +1, yk+1) y pk +1 = pk + 2Δy - 2Δx
-            
-        */ 
+        drawPixel(start.x, start.y);
+
+        if ((start.x === end.x) && (start.y === end.y)) {
+            break;
+        }
+
+        var e2 = 2 * pk; // 2 * Δpk
+
+        // Formulas del algoritmo de Bresenham
+
+        // Primera condición: e2 > -dy
+        if (e2 > -dy) {
+            pk -= dy;      // Δpk -= Δy
+            start.x += sx;  // x += sx
+        }
+
+        // Segunda condición: e2 < dx
+        if (e2 < dx) {
+            pk += dx;      // Δpk += Δx
+            start.y += sy;  // y += sy
+        }
+    }
+}
+
+
+    // y = mx + b
+    function drawLine2(start, end) {
+        // Ecuacion de la recta y = mx + b
+
+        var dx = end.x - start.x;        // Delta x
+        var dy = end.y - start.y;        // Delta y
+
+        // Pendiente
+        var m = dy / dx;
+        var b = start.y - m * start.x;   // Ordenada en el origen
+
+        // Intercambiar los puntos para que siempre se dibuje de izquierda a derecha
+        if (start.x > end.x) {
+            var temp = start;
+            start = end;
+            end = temp;
+        }
+
+        // Coordenada inicial
         var x = start.x;
         var y = start.y;
 
-        while (x != end.x) {
-            //Dependiendo del grosor dibujar mas pixeles alrederor del punto 
-            for (var i = 0; i < grosor; i++) {
-                for (var j = 0; j < grosor; j++) {
-                    ctx.fillStyle = color;
-                    ctx.fillRect(x + i, y + j, 1, 1);
-                }
-            }
+        // Determinar el cuadrante
+        var xIncrement, yIncrement;
 
-            var sx = (start.x < end.x) ? 1 : -1; // si el punto inicial es menor que el punto final entonces sx = 1, de lo contrario sx = -1
-            var sy = (start.y < end.y) ? 1 : -1; // si el punto inicial es menor que el punto final entonces sy = 1, de lo contrario sy = -1
-            
-            x += sx;
-            if (pk < 0) {
-                pk = pk + 2 * dy;
-            } else {
-                y += sy;
-                pk = pk + 2 * dy - 2 * dx;
-            }
+        if (Math.abs(m) <= 1) {
+            xIncrement = 1;
+            yIncrement = m;
+        } else {
+            xIncrement = 1 / Math.abs(m);
+            yIncrement = m < 0 ? -1 : 1;
+        }
+
+        //Dibujar los puntos intermedios
+        while (x <= end.x) {
+            drawPixel(Math.round(x), Math.round(y));
+            x += xIncrement;
+            y += yIncrement;
+        }
+
+        // Pintar el punto final
+        drawPixel(end.x, end.y);
+    }
+
+   // Dependiendo del grosor, dibujar más píxeles alrededor del punto 
+function drawPixel(x, y) {
+    var halfThickness = Math.floor(grosor / 2);
+
+    for (var i = -halfThickness; i <= halfThickness; i++) {
+        for (var j = -halfThickness; j <= halfThickness; j++) {
+            ctx.fillStyle = color;
+            ctx.fillRect(x + i, y + j, 1, 1);
         }
     }
-// y = mx + b
-function drawLine2(start, end) {
-    // Ecuacion de la recta y = mx + b
-
-    var dx = end.x - start.x;        // Delta x
-    var dy = end.y - start.y;        // Delta y
-
-    // Pendiente
-    var m = dy / dx;
-    var b = start.y - m * start.x;   // Ordenada en el origen
-
-    // Intercambiar los puntos para que siempre se dibuje de izquierda a derecha
-    if (start.x > end.x) {
-        var temp = start;
-        start = end;
-        end = temp;
-    }
-
-    // Coordenada inicial
-    var x = start.x;
-    var y = start.y;
-
-    // Determinar el cuadrante
-    var xIncrement, yIncrement;
-
-    if (Math.abs(m) <= 1) {
-        xIncrement = 1;
-        yIncrement = m;
-    } else {
-        xIncrement = 1 / Math.abs(m);
-        yIncrement = m < 0 ? -1 : 1;
-    }
-
-    //Dibujar los puntos intermedios
-    while (x <= end.x) {
-        drawPixel(Math.round(x), Math.round(y));
-        x += xIncrement;
-        y += yIncrement;
-    }
-
-    // Pintar el punto final
-    drawPixel(end.x, end.y);
 }
 
-    // Dependiendo del grosor, dibujar más píxeles alrededor del punto 
-    function drawPixel(x, y) {
-        for (var i = 0; i < grosor; i++) {
-            for (var j = 0; j < grosor; j++) {
-                ctx.fillStyle = color;
-                ctx.fillRect(x , y, 1, 1);
-            }
-        }
-    }
 
-
-   
+ 
     // DIGITAL DIFFERENTIAL ANALYZER
     function drawLineDDA(start, end) {
         // Algoritmo de DDA
@@ -244,13 +246,7 @@ function drawLine2(start, end) {
             var roundedY = Math.round(y);
 
             // Dependiendo del grosor, dibujar más píxeles alrededor del punto
-            for (var k = 0; k < grosor; k++) {
-                for (var j = 0; j < grosor; j++) {
-                    ctx.fillStyle = color;
-                    ctx.fillRect(roundedX + k, roundedY + j, 1, 1);
-                }
-            }
-
+            drawPixel(roundedX, roundedY);
             // Actualizar las coordenadas con el incremento
             x += xIncrement;
             y += yIncrement;
